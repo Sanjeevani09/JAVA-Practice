e9df134b1f4cd6783a909389304c9eaf6293b4a8
Sanjeevani Gupta <76159023+Sanjeevani09@users.noreply.github.com>
2024-12-09 23:43:36 +0530

    Create Pattern12.java

---
+++ Pattern12.java
@@ -0,0 +1,32 @@
+package pattern;
+
+import java.util.Scanner;
+
+public class Pattern12 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the number : ");
+		int num = sc.nextInt();
+		
+		for(int i = 1; i <= num; i++) {
+			for(int j = 1; j <= i; j++) {
+				System.out.print(j + " ");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
+
+
+//Pattern we want to print : 
+//
+//Enter the number : 5
+//
+//1 
+//2 2 
+//3 3 3 
+//4 4 4 4 
+//5 5 5 5 5
