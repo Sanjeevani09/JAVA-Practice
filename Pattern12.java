9cfbcb82e6cf13243ef03639e86f1da530d806d8
Sanjeevani Gupta <76159023+Sanjeevani09@users.noreply.github.com>
2024-12-28 15:32:39 +0530

    Update Pattern12.java

--- Pattern12.java
+++ Pattern12.java
@@ -11,7 +11,7 @@ public class Pattern12 {
 		
 		for(int i = 1; i <= num; i++) {
 			for(int j = 1; j <= i; j++) {
-				System.out.print(j + " ");
+				System.out.print(i+ " ");
 			}
 			System.out.println();
 		}
